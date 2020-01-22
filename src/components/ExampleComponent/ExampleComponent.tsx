101fddfabcd48819007b0753bc42d800a4a26a53
Jason Savelli <savellijason@gmail.com>
2020-01-22 21:30:31 +0100

    chore: add components folder

---
+++ src/components/ExampleComponent/ExampleComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import * as Styled from "./ExampleComponent.style";
+
+/**
+ * Composant servant d'exemple pour le react-starter
+ */
+export const ExampleComponent = () => {
+  return <Styled.Wrapper>ExampleComponent</Styled.Wrapper>;
+};
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+/** Dans ce dossier, sont exportés tout les dumb components */
+export * from "./ExampleComponent";
