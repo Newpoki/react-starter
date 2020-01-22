1de75e1204c8f1fac5fd3cfe8b6149adcd51f562
Jason Savelli <savellijason@gmail.com>
2020-01-22 23:44:27 +0100

    feat: add ExampleContainer in App and fix theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 
-import { ExampleComponent } from "components";
 import { GlobalStyle, theme } from "theme";
 import { ThemeProvider } from "styled-components";
+import { ExampleContainer } from "containers";
+import { Provider } from "react-redux";
+import { store } from "store";
 
 export const App = () => {
   return (
-    <div className="App">
-      <ThemeProvider theme={theme} />
-      <GlobalStyle />
-      <ExampleComponent />
+    <div className="App" style={{ display: "flex" }}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Provider store={store}>
+          <ExampleContainer />
+        </Provider>
+      </ThemeProvider>
     </div>
   );
 };
