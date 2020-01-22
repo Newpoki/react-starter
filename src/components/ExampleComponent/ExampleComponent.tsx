f90da9cdb6a5cd2b5e91b355c2f35400ea28706a
Jason Savelli <savellijason@gmail.com>
2020-01-22 23:33:13 +0100

    feat: add ExampleComponent prop and interface

--- src/components/ExampleComponent/ExampleComponent.tsx
+++ src/components/ExampleComponent/ExampleComponent.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+/** Imports NPM */
+import React, { memo } from "react";
 
+/** Import locaux */
 import * as Styled from "./ExampleComponent.style";
+import { IExampleComponentProps } from "./interface";
 
 /**
  * Composant servant d'exemple pour le react-starter
  */
-export const ExampleComponent = () => {
-  return <Styled.Wrapper>ExampleComponent</Styled.Wrapper>;
-};
+export const ExampleComponent = memo(({ someProp }: IExampleComponentProps) => (
+  <Styled.Wrapper>
+    <span>Valeur actuelle de la prop:</span>
+    <span>{someProp.toString()}</span>
+  </Styled.Wrapper>
+));
---
+++ src/components/ExampleComponent/interface.ts
@@ -0,0 +1,5 @@
+/** Interface du composant ExampleComponent */
+export interface IExampleComponentProps {
+  /** Boolean affiché en string par le composant */
+  someProp: boolean;
+}
