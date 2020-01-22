14153e71274278bdbe8f88d6f09095d5059865be
Jason Savelli <savellijason@gmail.com>
2020-01-23 00:06:57 +0100

    test: add ExampleReducerActions test

---
+++ src/store/actions/ExampleReducerActions/ExampleReducerActions.test.ts
@@ -0,0 +1,18 @@
+import { EXAMPLE_REDUCER_TYPE } from "store/actionTypes";
+import { exampleAction } from "./ExampleReducerActions";
+import { IExampleReducerPayload, IExampleReducerAction } from "store/reducers";
+
+describe("ExampleReducerActions", () => {
+  describe("exampleAction", () => {
+    it('should return the action that update "someConst" in exampleReducer', () => {
+      const payload: IExampleReducerPayload = false;
+
+      const expected: IExampleReducerAction = {
+        type: EXAMPLE_REDUCER_TYPE,
+        payload
+      };
+
+      expect(exampleAction(payload)).toEqual(expected);
+    });
+  });
+});
